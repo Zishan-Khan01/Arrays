3fe34fc2c719a86c61fb41d8aadb2f1efe41149d
Zishan Pathan <141132499+Zishan-Khan01@users.noreply.github.com>
2026-05-01 14:01:40 +0530

    Add files via upload

---
+++ MaxSubArraySumPS.java
@@ -0,0 +1,41 @@
+//Prefix Sum Approach
+//time comp=O(n square)
+
+public class MaxSubArraySumPS{
+	public static void maxSubArraySumPS(int numbers[]){
+		int currSum = 0;
+		int maxSum = Integer.MIN_VALUE;
+		int prefix[] = new int[numbers.length];
+		
+		prefix[0] = numbers[0];
+		//calculate prefix array
+		for(int i=1; i<numbers.length; i++){
+			prefix[i] = prefix[i-1] + numbers[i];
+		}
+		for(int i=0; i<numbers.length; i++){
+			int start = i;
+			for(int j=i; j<numbers.length; j++){
+				int end = j;
+				//what if start = 0
+				currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start -1];;
+				
+				//for(int k=start; k<=end; k++){
+				//	System.out.print(numbers[k]+" ");
+				//	//subArray sum
+				//	currSum += numbers[k];
+				//}
+				//System.out.println(": " + currSum);
+				
+				if(currSum > maxSum){
+					maxSum = currSum;
+				}
+			}
+		}
+		System.out.println("Max Sum: " + maxSum);
+	}
+
+	public static void main(String args[]){
+		int numbers[] = {2, 4, 6, 8, 10};
+		maxSubArraySumPS(numbers);		
+	}
+}
